c9e12a53c908efb92026f97749035f34bd10551f
beakkoungku <koungku93@gmail.com>
2021-02-26 22:44:28 +0900

    added mariaDBConnection

--- node_Server/main.js
+++ node_Server/main.js
@@ -1,9 +1,15 @@
 const express = require('express');
+var mdbConn = require('./mariaDBConnection');
+var router = express.Router();
 const app = express();
 const PORT = 5000;
 app.get('/', (req,res) => {
   res.send('Hello World!');
+  mdbConn.getUserList()
+  .then((rows)=> {res.json(rows)})
+  .catch((err)=> {console.log(err)})
 });
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);
 });
+
